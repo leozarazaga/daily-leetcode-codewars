32ecbc49f7b9839256cc348442827bcb72aca765
Leo Zarazaga <lza1313@outlook.com>
2024-07-31 17:03:53 +0200

    Runtime: 57ms

--- leetcode/001-ToBeOrNotToBe.js
+++ leetcode/001-ToBeOrNotToBe.js
@@ -10,11 +10,15 @@ Example 1:
 Input: func = () => expect(5).toBe(5)
 Output: {"value": true}
 Explanation: 5 === 5 so this expression returns true.
+
+
 Example 2:
 
 Input: func = () => expect(5).toBe(null)
 Output: {"error": "Not Equal"}
 Explanation: 5 !== null so this expression throw the error "Not Equal".
+
+
 Example 3:
 
 Input: func = () => expect(5).notToBe(null)
