b90818c11145edff1eb7a589309d55b4b519a46d
Leo Zarazaga <lza1313@outlook.com>
2024-08-10 16:44:41 +0200

    Time: 806ms

---
+++ codewars/010-WhatIsBetween.js
@@ -0,0 +1,24 @@
+/*********************************************************************************************
+
+"What is between?"
+
+Complete the function that takes two integers (a, b, where a < b)
+and return an array of all integers between the input parameters,
+including them.
+
+For example:
+    a = 1
+    b = 4
+    --> [1, 2, 3, 4]
+   
+********************************************************************************************/
+
+function between(a, b) {
+    let array = [];
+    for (let i = a; i <= b; i++) {
+        array.push(i)
+    }
+    return array;
+}
+
+console.log(between(1, 4));
