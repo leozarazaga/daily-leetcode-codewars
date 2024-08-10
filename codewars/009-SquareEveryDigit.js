3f3b4fe008746369beda874403e70f774328c9cc
Leo Zarazaga <lza1313@outlook.com>
2024-08-10 16:35:07 +0200

    Time: 812ms

---
+++ codewars/009-SquareEveryDigit.js
@@ -0,0 +1,22 @@
+/*********************************************************************************************
+
+"Square Every Digit"
+
+Welcome. In this kata, you are asked to square every digit of a
+number and concatenate them. For example, if we run 9119 through
+the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
+
+Example #2:
+An input of 765 will/should return 493625
+because 7^2 is 49, 6^2 is 36, and 5^2 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+   
+********************************************************************************************/
+
+function squareDigits(num) {
+    let numbers = num.toString();
+    return parseInt(numbers.split("").map(value => Math.pow(parseInt(value), 2)).join(""));
+}
+
+console.log(squareDigits(9119));
