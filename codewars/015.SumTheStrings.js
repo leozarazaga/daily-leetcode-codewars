b16adb2885da005708647159519868e82f31d3bc
Leo Zarazaga <lza1313@outlook.com>
2024-08-17 22:01:45 +0200

    Time: 765ms

---
+++ codewars/015.SumTheStrings.js
@@ -0,0 +1,25 @@
+/*********************************************************************************************
+
+"Sum The Strings"
+
+Create a function that takes 2 integers in form of a string
+as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 --> Output)
+    "4",  "5" --> "9"
+    "34", "5" --> "39"
+    "", "" --> "0"
+    "2", "" --> "2"
+    "-5", "3" --> "-2"
+
+********************************************************************************************/
+
+function sumStr(a, b) {
+    let num1 = parseInt(a || '0');
+    let num2 = parseInt(b || '0');
+    return (num1 + num2).toString();
+}
+
+console.log(sumStr("4", "5"));
+console.log(sumStr("34", "5"));
+console.log(sumStr("", ""));
